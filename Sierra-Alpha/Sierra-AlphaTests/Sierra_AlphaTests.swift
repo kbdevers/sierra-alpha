091870d4959370f46d0df0f4a9c1b9de203eac81
Maurus Kühne <mauruskuehne@icloud.com>
2017-06-22 20:25:49 +0200

    created xcode project

---
+++ Sierra-Alpha/Sierra-AlphaTests/Sierra_AlphaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sierra_AlphaTests.swift
+//  Sierra-AlphaTests
+//
+//  Created by Maurus Kühne on 22.06.17.
+//  Copyright © 2017 Maurus Kühne. All rights reserved.
+//
+
+import XCTest
+@testable import Sierra_Alpha
+
+class Sierra_AlphaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
